e63dadb7f51648ebea818b9f3c2be4d37e046804
Cyprian Zdebski <cyprian.zdebski@protonmail.com>
2023-05-04 15:04:40 +0200

    Działająca rejestracja i logowanie

---
+++ Plantasia/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Plantasia.Areas.Identity.Data;
+using Plantasia.Models;
+
+namespace Plantasia.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pet>().HasKey(m => m.Id);
+        modelBuilder.Entity<Pet>().HasData(GetPets());
+        base.OnModelCreating(modelBuilder);
+    }
+    private List<Pet> GetPets()
+    {
+        return Enumerable.Range(1, 10)
+            .Select(index => new Pet
+            {
+                Id = index,
+                Level = index,
+                Exp = 1327*index,
+                RequiredExp = 5000*index,
+                PictureHref = ".",
+                Produce = $"PRODUCE{index}"
+            })
+            .ToList();
+    }
+}
--- Plantasia/Program.cs
+++ Plantasia/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Plantasia.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseInMemoryDatabase("MemoryDb"));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,5 +32,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
