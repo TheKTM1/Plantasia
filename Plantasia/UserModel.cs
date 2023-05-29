e8a52c8110be0ae9df241b06d7532be261792681
Krzysztof Gola <kripsu2@gmail.com>
2023-05-30 00:49:45 +0200

    Nie chce się kompilować
    test

---
+++ Plantasia/UserModel.cs
@@ -0,0 +1,9 @@
+﻿internal class UserModel
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string ID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool EmailConfirmed { get; set; }
+}
